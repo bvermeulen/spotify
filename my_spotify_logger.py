455e0780537e4f8485bb4b6421206c8d72105769
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2021-06-22 17:03:33 +0700

    initial commit

---
+++ my_spotify_logger.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass, asdict
+import time
+import datetime
+from pathlib import Path
+import json
+from decouple import config
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+TRACK_LOGFILE= Path('logs/track_log.json')
+TIME_DELAY = 5
+SCOPE = 'user-read-currently-playing'
+SPOTIFY_CLIENT_ID = config('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = config('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_REDIRECT_URI = config('SPOTIFY_REDIRECT_URI')
+
+spotify_authorization = SpotifyOAuth(
+    SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET,
+    SPOTIFY_REDIRECT_URI,
+    scope=SCOPE,
+)
+spotify = spotipy.Spotify(auth_manager=spotify_authorization)
+
+
+@dataclass
+class TrackRecord():
+    played_at: str
+    name: str
+    id: int
+    artist: str
+
+    def as_dict(self):
+        return asdict(self)
+
+
+def read_track_logfile():
+    with open(TRACK_LOGFILE, 'r') as json_file:
+        return json.load(json_file)
+
+
+def update_track_logfile(tracks_log):
+    with open(TRACK_LOGFILE, 'w') as json_file:
+        json.dump(tracks_log, json_file)
+
+
+def print_track(track):
+    print(f'{track.played_at}, id: {track.id}\n'
+          f'artist: {track.artist}, {track.name}')
+
+
+def main():
+    tracks_log = read_track_logfile()
+    track_id = None
+
+    while True:
+        new_track = spotify.current_user_playing_track()
+
+        if new_track and track_id != new_track['item']['id']:
+            track_id = new_track['item']['id']
+            track_name = new_track['item']['name']
+            track_artist = new_track['item']['artists'][0]['name']
+            track_played_at = datetime.datetime.now()
+
+            track_record = TrackRecord(
+                played_at=track_played_at.strftime("%Y-%B-%d %H:%M:%S"),
+                id=track_id,
+                artist=track_artist,
+                name=track_name,
+            )
+            # print_track(track_record)
+            tracks_log['tracks'].append(track_record.as_dict())
+            update_track_logfile(tracks_log)
+
+        time.sleep(TIME_DELAY)
+
+if __name__ == '__main__':
+    main()
